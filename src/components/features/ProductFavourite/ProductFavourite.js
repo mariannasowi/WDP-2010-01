72122dd0f8146ec98311f3fe1eede7e1a382c1ae
Edyta Wojdala <edytawoj095@gmail.com>
2020-10-10 22:49:43 +0200

    Add addToFavouriteHandler

--- src/components/features/ProductFavourite/ProductFavourite.js
+++ src/components/features/ProductFavourite/ProductFavourite.js
@@ -6,19 +6,18 @@ import Button from '../../common/Button/Button';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
 const ProductFavourite = ({ id, heart, addToFavourite, removeFromFavourite }) => {
+  const addToFavouriteHandler = event => {
+    event.preventDefault();
+  };
+
   return (
     <div
       onClick={() => {
         heart ? removeFromFavourite({ id }) : addToFavourite({ id });
       }}
     >
-      <Button variant='outline'>
-        <FontAwesomeIcon
-          id={id}
-          heart={heart}
-          icon={faHeart}
-          className={heart ? style.heart : ''}
-        >
+      <Button variant='outline' onClick={addToFavouriteHandler}>
+        <FontAwesomeIcon id={id} icon={faHeart} className={heart ? style.heart : ''}>
           Favorite
         </FontAwesomeIcon>
       </Button>
