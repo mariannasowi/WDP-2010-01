0642a07d2648b105c3b3f339078717470c19f7a6
Tomasz Szabla <111thomas1111@gmail.com>
2020-10-11 11:59:46 +0200

    WDP201001-19

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -3,68 +3,104 @@ import PropTypes from 'prop-types';
 
 import styles from './ProductBox.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faStar,
-  faExchangeAlt,
-  faShoppingBasket,
-} from '@fortawesome/free-solid-svg-icons';
-import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faExchangeAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
+import ProductRating from '../../features/ProductRating/ProductRatingContainer';
 
-const ProductBox = ({ name, image, price, oldPrice, promo, stars }) => (
-  <div className={styles.root}>
-    <div className={styles.photo}>
-      <img src={image} alt={name} />
-      {promo && <div className={styles.sale}>{promo}</div>}
-      <div className={styles.buttons}>
-        <Button variant='small'>Quick View</Button>
-        <Button variant='small'>
-          <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
-        </Button>
+const ProductBox = ({
+  id,
+  name,
+  image,
+  price,
+  oldPrice,
+  promo,
+  stars,
+  isStarred,
+  setCompare,
+  count,
+  compare,
+  heart,
+  addCompare,
+}) => {
+  const compareHandler = event => {
+    event.preventDefault();
+    const maxProductsToCompare = 4;
+    const isProductAddedToCompare =
+      compare.products &&
+      compare.products.reduce(
+        (accumulator, product) => accumulator || product.id === id,
+        false
+      );
+
+    if (isProductAddedToCompare !== true) {
+      count < maxProductsToCompare
+        ? setCompare({ id, image })
+        : alert(`You can compare maximum of ${maxProductsToCompare} products!`);
+    }
+  };
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.photo}>
+        <img src={image} alt={name} />
+        {promo && <div className={styles.sale}>{promo}</div>}
+        <div className={styles.buttons}>
+          <Button variant='small'>Quick View</Button>
+          <Button variant='small'>
+            <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
+          </Button>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <h5>{name}</h5>
+        <div className={styles.stars}>
+          <ProductRating id={id} stars={stars} isStarred={isStarred} />
+        </div>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.actions}>
+        <div className={styles.outlines}>
+          <Button variant='outline'>
+            <FontAwesomeIcon icon={faHeart} className={heart ? styles.heart : ''}>
+              Favorite
+            </FontAwesomeIcon>
+          </Button>
+          <Button variant='outline' onClick={compareHandler}>
+            <FontAwesomeIcon
+              icon={faExchangeAlt}
+              className={addCompare ? styles.addCompare : ''}
+            >
+              Add to compare
+            </FontAwesomeIcon>
+          </Button>
+        </div>
+        <div className={styles.oldPrice}>{oldPrice}</div>
+        <div className={styles.price}>
+          <Button noHover variant='small'>
+            $ {price}
+          </Button>
+        </div>
       </div>
     </div>
-    <div className={styles.content}>
-      <h5>{name}</h5>
-      <div className={styles.stars}>
-        {[1, 2, 3, 4, 5].map(i => (
-          <a key={i} href='#'>
-            {i <= stars ? (
-              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
-            ) : (
-              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
-            )}
-          </a>
-        ))}
-      </div>
-    </div>
-    <div className={styles.line}></div>
-    <div className={styles.actions}>
-      <div className={styles.outlines}>
-        <Button variant='outline'>
-          <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
-        </Button>
-        <Button variant='outline'>
-          <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
-        </Button>
-      </div>
-      <div className={styles.oldPrice}>{oldPrice}</div>
-      <div className={styles.price}>
-        <Button noHover variant='small'>
-          $ {price}
-        </Button>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 ProductBox.propTypes = {
   children: PropTypes.node,
+  id: PropTypes.string.isRequired,
   name: PropTypes.string,
   price: PropTypes.number,
   oldPrice: PropTypes.string,
   promo: PropTypes.string,
   stars: PropTypes.number,
+  isStarred: PropTypes.bool,
+  heart: PropTypes.bool,
+  addCompare: PropTypes.bool,
   image: PropTypes.string,
+  setCompare: PropTypes.func.isRequired,
+  count: PropTypes.number.isRequired,
+  compare: PropTypes.object.isRequired,
 };
 
 export default ProductBox;
