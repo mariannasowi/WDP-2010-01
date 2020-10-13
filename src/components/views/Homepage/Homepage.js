822ccea06c5f311c57a286a3eb078028516472a8
Tomasz Szabla <111thomas1111@gmail.com>
2020-10-13 23:52:41 +0200

    WDP201001-33 fix conflict

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -6,10 +6,12 @@ import styles from './Homepage.module.scss';
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import ClientFeedback from '../../features/ClientFeedback/ClientFeedbackContainer';
+import PromotedProduct from '../../features/PromotedProduct/PromotedProductContainer';
 import SalesContainer from '../../features/Sales/SalesContainer';
 
 const Homepage = () => (
   <div className={styles.root}>
+    <PromotedProduct />
     <FeatureBoxes />
     <SalesContainer />
     <NewFurniture />
