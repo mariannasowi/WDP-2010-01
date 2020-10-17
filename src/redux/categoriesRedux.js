d9dd40e9c7bc055e2a045c2ba5c39626fd060f98
Maciek Lubocki <mail2maciek@gmail.com>
2020-10-17 23:23:18 +0200

    Styling updates

--- src/redux/categoriesRedux.js
+++ src/redux/categoriesRedux.js
@@ -1,7 +1,6 @@
 /* selectors */
 export const getAll = ({ categories }) => categories;
 export const getCount = ({ categories }) => categories.length;
-export const getGalleryCategory = ({ galleryCategory }) => galleryCategory;
 
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
