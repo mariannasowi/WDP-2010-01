5e88869e063a5a44b02d30b32b8c873950c96064
Edyta Wojdala <edytawoj095@gmail.com>
2020-10-18 20:35:14 +0200

    Change navbar style

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -70,20 +70,29 @@ const ProductPage = ({
   else
     return (
       <div className={styles.root}>
-        <div className={'col-auto ' + styles.menu}>
+        <div className={styles.menu}>
           <div className='container'>
-            <ul>
-              <h2>FURNITURE</h2>
-              <li>
-                <a href='/'>Home</a>
-              </li>
-              <li>
-                <a href='/#'>Furniture </a>
-              </li>
-              <li>
-                <a href='/#'>{category} </a>
-              </li>
-            </ul>
+            <div>
+              <div className='navbar'>
+                <h2>FURNITURE</h2>
+              </div>
+              <div>
+                <ul>
+                  <li>
+                    <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
+                    <a href='/'>Home</a>
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
+                    <a href='/#'>Furniture </a>
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
+                    <a href='/#'>{category} </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
         <div className='container'>
