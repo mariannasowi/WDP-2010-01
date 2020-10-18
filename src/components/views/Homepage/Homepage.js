e16ba18acf95476dce479b5322ee79f25942a6f8
Maciek Lubocki <mail2maciek@gmail.com>
2020-10-18 23:10:41 +0200

    Conflict resolved on Homepage.js imports

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -5,12 +5,9 @@ import styles from './Homepage.module.scss';
 
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
-<<<<<<< HEAD
 import Gallery from '../../features/Gallery/GalleryContainer';
-=======
 import ClientFeedback from '../../features/ClientFeedback/ClientFeedbackContainer';
 
->>>>>>> master
 import PromotedProduct from '../../features/PromotedProduct/PromotedProductContainer';
 import SalesContainer from '../../features/Sales/SalesContainer';
 import LatestBlog from '../../features/LatestBlog/LatestBlogContainer';
