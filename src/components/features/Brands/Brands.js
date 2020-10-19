3f2fea9fb1f5190f2101bc5f651d5ffac0f18ec9
Marianna Sowinska <marianna.sowi@gmail.com>
2020-10-19 19:50:19 +0200

    Fix Swipe, deleted lines after code review

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -22,65 +22,30 @@ class Brands extends React.Component {
     }));
   }
 
-  getBrandsCountFromMode(mode) {
-    switch (mode) {
-      case 'laptop':
-        return 4;
-      case 'tablet':
-        return 3;
-      case 'mobile':
-        return 2;
-      default:
-        return 6;
-    }
-  }
-
   render() {
-    const { brands, mode } = this.props;
-
-    const brandsNumberOnSlide = this.getBrandsCountFromMode(mode);
-    const pagesCount = Math.ceil(brands.length / brandsNumberOnSlide);
-
-    const pages = [];
-    for (let i = 0; i < pagesCount; i++) {
-      pages.push(
-        <div className={'row' + ' ' + styles.brandsElement}>
-          {brands
-            .slice(i * brandsNumberOnSlide, (i + 1) * brandsNumberOnSlide)
-            .map(brand => (
-              <div key={brand.id} className={'col-6 col-sm-3 col-xl-2'}>
-                <Brands {...brand} />
-              </div>
-            ))}
-        </div>
-      );
-    }
-
-    const renderPages = () => {
-      return pages;
-    };
+    const { brands } = this.props;
+    const { activePage } = this.state;
 
     return (
       <div className={styles.root}>
         <div className='container'>
-          <SwipeComponent
-            activePage={this.state.activePage}
-            rightAction={this.handleRightAction}
-            leftAction={this.handleLeftAction}
-          >
-            {renderPages}
-          </SwipeComponent>
           <div className={`row ${styles.wrapper}`}>
             <div className={styles.arrow}>
               <FontAwesomeIcon className={styles.icon} icon={faAngleLeft} />
             </div>
-            <div className={styles.brandBox}>
-              {brands.map(brand => (
-                <div key={brand.id} className={styles.box}>
-                  <img src={brand.image} alt={brand.name} />
-                </div>
-              ))}
-            </div>
+            <SwipeComponent
+              activePage={this.state.activePage}
+              rightAction={this.handleRightAction}
+              leftAction={this.handleLeftAction}
+            >
+              <div className={styles.brandBox}>
+                {brands.slice(activePage, activePage + 6).map(brand => (
+                  <div key={brand.id} className={styles.box}>
+                    <img src={brand.image} alt={brand.name} />
+                  </div>
+                ))}
+              </div>
+            </SwipeComponent>
             <div className={styles.arrow}>
               <FontAwesomeIcon className={styles.icon} icon={faAngleRight} />
             </div>
@@ -93,7 +58,6 @@ class Brands extends React.Component {
 
 Brands.propTypes = {
   brands: PropTypes.array,
-  mode: PropTypes.string,
   id: PropTypes.string,
   name: PropTypes.string,
   image: PropTypes.string,
