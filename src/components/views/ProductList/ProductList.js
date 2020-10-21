6b9db91721910c207a2762d31c98bf2bf5ec4673
Edyta Wojdala <edytawoj095@gmail.com>
2020-10-21 17:23:28 +0200

    Create component ProductListImages

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -5,39 +5,14 @@ import ProductBox from '../../common/ProductBox/ProductBox';
 import PageNotFound from '../../views/PageNotFound/PageNotFound';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import ProductListImages from '../ProductListImages/ProductListImages';
 
 const ProductList = ({ id, name, products, error }) => {
   if (error) return <PageNotFound />;
   else
     return (
       <div>
-        <div className='navbar'>
-          <h2>{name}</h2>
-        </div>
-        <div>
-          <ul>
-            <li>
-              <a href='/'>Home</a>
-            </li>
-          </ul>
-        </div>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-9'>
-              <div className='row'>
-                {products &&
-                  products
-                    .filter(item => item.category === id)
-                    .map((product, i) => (
-                      <div key={i} className='col-4'>
-                        <ProductBox {...product} />
-                      </div>
-                    ))}
-              </div>
-            </div>
-            <div className='col-3'></div>
-          </div>
-        </div>
+        <ProductListImages />
       </div>
     );
 };
---
+++ src/components/views/ProductListImages/ProductListImages.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ProductBox from '../../common/ProductBox/ProductBox';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+const ProductListImages = ({ id, name, products}) => {
+  return (
+    <div>
+      <div className='navbar'>
+        <h2>{name}</h2>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <a href='/'>Home</a>
+          </li>
+        </ul>
+      </div>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-9'>
+            <div className='row'>
+              {products &&
+                  products
+                    .filter(item => item.category === id)
+                    .map((product, i) => (
+                      <div key={i} className='col-4'>
+                        <ProductBox {...product} />
+                      </div>
+                    ))}
+            </div>
+          </div>
+          <div className='col-3'></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ProductListImages.propTypes = {
+  category: PropTypes.string,
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      category: PropTypes.string,
+      price: PropTypes.number,
+      stars: PropTypes.number,
+      promo: PropTypes.string,
+      newFurniture: PropTypes.bool,
+    })
+  ),
+};
+
+export default ProductListImages;
