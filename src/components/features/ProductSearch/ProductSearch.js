9e85dd74b86c0a18a50f4640c388319ca554906f
Maciek Lubocki <mail2maciek@gmail.com>
2020-10-21 15:23:35 +0200

    Task 14 updates

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -8,60 +8,60 @@ import styles from './ProductSearch.module.scss';
 
 const ProductSearch = () => (
   <div className={styles.center}>
-  <form action='' className={styles.root}>
-    <div className={styles.category}>
-      <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-      <ul className={styles.list}>
-        <li className={styles.listCategory}>
-          <span value=''>Select a category</span>
-          <ul className={styles.listCategoryItems}>
-            <li>
-              <a href='#' value=''>
-                HOME
-              </a>
-            </li>
-            <li>
-              <a href='#' value=''>
-                FURNITURE
-              </a>
-            </li>
-            <li>
-              <a href='#' value=''>
-                CHAIR
-              </a>
-            </li>
-            <li>
-              <a href='#' value=''>
-                TABLE
-              </a>
-            </li>
-            <li>
-              <a href='#' value=''>
-                SOFA
-              </a>
-            </li>
-            <li>
-              <a href='#' value=''>
-                BEDROOM
-              </a>
-            </li>
-            <li>
-              <a href='#' value=''>
-                BLOG
-              </a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-      <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
-    </div>
-    <div className={styles.searchField}>
-      <input placeholder='Search products...' type='text' />
-      <button>
-        <FontAwesomeIcon className={styles.icon} icon={faSearch} />
-      </button>
-    </div>
-  </form>
+    <form action='' className={styles.root}>
+      <div className={styles.category}>
+        <FontAwesomeIcon className={styles.icon} icon={faListUl} />
+        <ul className={styles.list}>
+          <li className={styles.listCategory}>
+            <span value=''>Select a category</span>
+            <ul className={styles.listCategoryItems}>
+              <li>
+                <a href='#' value=''>
+                  HOME
+                </a>
+              </li>
+              <li>
+                <a href='#' value=''>
+                  FURNITURE
+                </a>
+              </li>
+              <li>
+                <a href='#' value=''>
+                  CHAIR
+                </a>
+              </li>
+              <li>
+                <a href='#' value=''>
+                  TABLE
+                </a>
+              </li>
+              <li>
+                <a href='#' value=''>
+                  SOFA
+                </a>
+              </li>
+              <li>
+                <a href='#' value=''>
+                  BEDROOM
+                </a>
+              </li>
+              <li>
+                <a href='#' value=''>
+                  BLOG
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+      </div>
+      <div className={styles.searchField}>
+        <input placeholder='Search products...' type='text' />
+        <button>
+          <FontAwesomeIcon className={styles.icon} icon={faSearch} />
+        </button>
+      </div>
+    </form>
   </div>
 );
 
