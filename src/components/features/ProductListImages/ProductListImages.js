e17c725ad0101efd67222c092b1321201d2503e0
Maciek Lubocki <mail2maciek@gmail.com>
2020-10-24 12:02:26 +0200

    Insert ControlBar to ProductList Component

--- src/components/features/ProductListImages/ProductListImages.js
+++ src/components/features/ProductListImages/ProductListImages.js
@@ -2,10 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ProductBox from '../../common/ProductBox/ProductBoxContainer';
 
-const ProductListImages = ({ id, products, name }) => {
+const ProductListImages = ({ id, products }) => {
   return (
     <div>
-      <h2>{name}</h2>
       <div className='container'>
         <div className='row'>
           <div className='col'>
@@ -29,7 +28,6 @@ const ProductListImages = ({ id, products, name }) => {
 ProductListImages.propTypes = {
   category: PropTypes.string,
   id: PropTypes.string,
-  name: PropTypes.string,
   products: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string,
--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PageNotFound from '../../views/PageNotFound/PageNotFound';
 import ProductListImages from '../../features/ProductListImages/ProductListImagesContainer';
+import ControlBar from '../../common/ControlBar/ControlBar';
+import styles from './ProductList.module.scss';
 
 const ProductList = ({ error }) => {
   if (error) return <PageNotFound />;
   else
     return (
       <div className='container'>
+        <div className='row'>
+          <div className={styles.topControlbar}>
+            <h2>Furniture</h2>
+            <ControlBar />
+          </div>
+        </div>
         <div className='row'>
           <div className='col-9'>
             <ProductListImages />
